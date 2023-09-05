545bcc557d67920b651b820c070f16228be2fb88
rachmad <33402690+rachmadvwp@users.noreply.github.com>
2023-09-05 13:02:09 +0200

    Create squareroot.c
    Create an initial code of the squareroot.c

---
+++ squareroot.c
@@ -0,0 +1,35 @@
+// Author: Rachmad V. W. Putra
+// Description: Model of a square root computation from the paper "A Novel Fixed-Point Square Root Algorithm and Its Digital Hardware Design" (https://doi.org/10.1109/ICTSS.2013.6588110)
+
+#include <stdio.h>
+
+int main(void)
+{
+  // Example
+  int D = 445;
+  int n = 10;
+  
+  // Initiation
+  int R = 0, Q = 0, F = 0;
+  int mask = 0x3;
+  int Ft = 0;
+   
+  for (int i = n; i > 0; i -= 2) 
+  {
+	  R = (R << 2) | (D & ( 3 << (i-2) )) >> (i-2);
+    if (R >= ((F << 1) | 1)) 
+	  {
+      Q = (Q << 1) | 1;
+      F = ((F + (F & 1)) << 1) | 1;
+    } 
+	  else 
+	  {
+      Q = (Q << 1) | 0;
+      F = ((F + (F & 1)) << 1) | 0;
+    }
+    R = R - (F * (F & 1));
+	  
+	  printf("index(%d) = %d/%d\n", i, Q, R);
+   }
+   printf("sqrt(%d) = %d/%d\n", D, Q, R);
+}
