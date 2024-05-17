5c032488604d88791eb49d1db0ab4a30e927e217
rvp2042 <rvp2042@nyu.edu>
2024-05-17 12:16:00 +0400

    coba

--- squareroot.c
+++ squareroot.c
@@ -1,6 +1,7 @@
 // Author: Rachmad V. W. P.
 // Description: Model of a square root computation from the paper "A Novel Fixed-Point Square Root Algorithm and Its Digital Hardware Design" 
 // Link of the paper: https://doi.org/10.1109/ICTSS.2013.6588110
+// 
  
 #include <stdio.h>
 
