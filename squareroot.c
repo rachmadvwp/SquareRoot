e4857466ee8ff4ebba39154fc2a93d85b3f8b42d
rachmad <33402690+rachmadvwp@users.noreply.github.com>
2024-06-03 15:13:52 +0400

    Update squareroot.c
    Update squareroot.c

--- squareroot.c
+++ squareroot.c
@@ -1,7 +1,6 @@
-// Author: Rachmad V. W. P.
+// Author: Rachmad V. W. Putra
 // Description: Model of a square root computation from the paper "A Novel Fixed-Point Square Root Algorithm and Its Digital Hardware Design" 
 // Link of the paper: https://doi.org/10.1109/ICTSS.2013.6588110
-// 
  
 #include <stdio.h>
 
